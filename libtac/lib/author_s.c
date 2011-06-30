ba6ee72ac07d10cc05660f8e1a0bcfb2f4613796
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2011-06-30 14:31:06 -0300

    Refactor authorization: - send authentication method in authen_method field - send packet as one (not fragmented) - fix indentation - remove commented code

--- libtac/lib/author_s.c
+++ libtac/lib/author_s.c
@@ -27,80 +27,101 @@
 #include <unistd.h>
 
 #ifndef __linux__
-  #include <strings.h>
+#include <strings.h>
 #endif
 
 #include "tacplus.h"
 #include "libtac.h"
 #include "xalloc.h"
 
+#include <librouter/defines.h>
+#include <librouter/pam.h>
+
 /* Send authorization request to the server, along with attributes
-   specified in attribute list prepared with tac_add_attrib.
-*/
-int tac_author_send(int fd, const char *user, char *tty, struct tac_attrib *attr) {
+ specified in attribute list prepared with tac_add_attrib.
+ */
+int tac_author_send(int fd, const char *user, char *tty, struct tac_attrib *attr)
+{
 	HDR *th;
 	struct author tb;
 	u_char user_len, port_len;
 	struct tac_attrib *a;
-	int i = 0; 			/* attributes count */
-	int pkt_len = 0; 	/* current packet length */
-	int pktl = 0;		/* temporary storage for previous pkt_len values */
-	int w; 				/* write() return value */
-	u_char *pkt;		/* packet building pointer */
-	/* u_char *pktp; */		/* obsolete */
+	int i = 0; /* attributes count */
+	int pkt_len = 0; /* current packet length */
+	int pktl = 0; /* temporary storage for previous pkt_len values */
+	int w; /* write() return value */
+	u_char *pkt; /* packet building pointer */
+	/* u_char *pktp; *//* obsolete */
 	int ret = 0;
+	unsigned char *data, *d;
+	int mode;
 
-	th=_tac_req_header(TAC_PLUS_AUTHOR);
+	th = _tac_req_header(TAC_PLUS_AUTHOR);
 
 	/* set header options */
- 	th->version=TAC_PLUS_VER_0;
- 	th->encryption=tac_encryption ? TAC_PLUS_ENCRYPTED : TAC_PLUS_CLEAR;
+	th->version = TAC_PLUS_VER_0;
+	th->encryption = tac_encryption ? TAC_PLUS_ENCRYPTED : TAC_PLUS_CLEAR;
 
-	TACDEBUG((LOG_DEBUG, "%s: user '%s', tty '%s', encrypt: %s", \
-		       	__FUNCTION__, user, \
-			tty, tac_encryption ? "yes" : "no"))
-	
-	user_len=(u_char) strlen(user);
-	port_len=(u_char) strlen(tty);
+	syslog(LOG_DEBUG, "%s: user '%s', tty '%s', encrypt: %s",
+					__FUNCTION__, user,
+					tty, tac_encryption ? "yes" : "no");
 
-	tb.authen_method=AUTHEN_METH_TACACSPLUS;
-	tb.priv_lvl=TAC_PLUS_PRIV_LVL_MIN;
-	if(strcmp(tac_login,"chap") == 0) {
-		tb.authen_type=TAC_PLUS_AUTHEN_TYPE_CHAP;
-	} else if(strcmp(tac_login,"login") == 0) {
-		tb.authen_type=TAC_PLUS_AUTHEN_TYPE_ASCII;
-	} else {
-		tb.authen_type=TAC_PLUS_AUTHEN_TYPE_PAP;
+	user_len = (u_char) strlen(user);
+	port_len = (u_char) strlen(tty);
+
+	mode = librouter_pam_get_current_mode(FILE_PAM_LOGIN);
+	switch (mode) {
+	case AAA_AUTH_NONE:
+		tb.authen_method = AUTHEN_METH_NONE;
+		break;
+	case AAA_AUTH_LOCAL:
+		tb.authen_method = AUTHEN_METH_LOCAL;
+		break;
+	case AAA_AUTH_RADIUS:
+	case AAA_AUTHOR_RADIUS_LOCAL:
+		tb.authen_method = AUTHEN_METH_RADIUS;
+		break;
+	case AAA_AUTH_TACACS:
+	case AAA_AUTH_TACACS_LOCAL:
+	default:
+		tb.authen_method = AUTHEN_METH_TACACSPLUS;
+		break;
 	}
 
-	if (!strcmp(user, "$enable$"))
-		tb.service=TAC_PLUS_AUTHEN_SVC_NONE;
-	else
-		tb.service=TAC_PLUS_AUTHEN_SVC_LOGIN;
+	tb.priv_lvl = TAC_PLUS_PRIV_LVL_MIN;
+	if (strcmp(tac_login, "chap") == 0) {
+		tb.authen_type = TAC_PLUS_AUTHEN_TYPE_CHAP;
+	} else if (strcmp(tac_login, "login") == 0) {
+		tb.authen_type = TAC_PLUS_AUTHEN_TYPE_ASCII;
+	} else {
+		tb.authen_type = TAC_PLUS_AUTHEN_TYPE_PAP;
+	}
 
-	tb.user_len=user_len;
-	tb.port_len=port_len;
-	tb.rem_addr_len=0;
+	tb.service = TAC_PLUS_AUTHEN_SVC_NONE;
+
+	tb.user_len = user_len;
+	tb.port_len = port_len;
+	tb.rem_addr_len = 0;
 
 	/* allocate packet */
-	pkt=(u_char *) xcalloc(1, TAC_AUTHOR_REQ_FIXED_FIELDS_SIZE);
-	pkt_len=sizeof(tb);
+	pkt = (u_char *) xcalloc(1, TAC_AUTHOR_REQ_FIXED_FIELDS_SIZE);
+	pkt_len = sizeof(tb);
 
 	/* fill attribute length fields */
 	a = attr;
-	while(a) {
-		
+	while (a) {
+
 		pktl = pkt_len;
 		pkt_len += sizeof(a->attr_len);
 		pkt = xrealloc(pkt, pkt_len);
 
 		/* bad method: realloc() is allowed to return different pointer
-		   with each call
-		pktp=pkt + pkt_len; 
-		pkt_len += sizeof(a->attr_len);
-		pkt = xrealloc(pkt, pkt_len);	
-		*/
-				
+		 with each call
+		 pktp=pkt + pkt_len;
+		 pkt_len += sizeof(a->attr_len);
+		 pkt = xrealloc(pkt, pkt_len);
+		 */
+
 		bcopy(&a->attr_len, pkt + pktl, sizeof(a->attr_len));
 		i++;
 
@@ -110,13 +131,6 @@ int tac_author_send(int fd, const char *user, char *tty, struct tac_attrib *attr
 	/* fill the arg count field and add the fixed fields to packet */
 	tb.arg_cnt = i;
 	bcopy(&tb, pkt, TAC_AUTHOR_REQ_FIXED_FIELDS_SIZE);
-/*
-#define PUTATTR(data, len) \
-	pktp = pkt + pkt_len; \
-	pkt_len += len; \
-	pkt = xrealloc(pkt, pkt_len); \
-	bcopy(data, pktp, len);
-*/
 
 #define PUTATTR(data, len) \
 	pktl = pkt_len; \
@@ -130,7 +144,7 @@ int tac_author_send(int fd, const char *user, char *tty, struct tac_attrib *attr
 
 	/* fill attributes */
 	a = attr;
-	while(a) {
+	while (a) {
 		PUTATTR(a->attr, a->attr_len)
 
 		a = a->next;
@@ -139,32 +153,25 @@ int tac_author_send(int fd, const char *user, char *tty, struct tac_attrib *attr
 	/* finished building packet, fill len_from_header in header */
 	th->datalength = htonl(pkt_len);
 
-	/* write header */
- 	w=write(fd, th, TAC_PLUS_HDR_SIZE);
-
-	if(w < TAC_PLUS_HDR_SIZE) {
-		syslog(LOG_ERR, "%s: author hdr send failed: wrote %d of %d", 
-				__FUNCTION__, w,
-				TAC_PLUS_HDR_SIZE);
-		free(pkt);
-		free(th);
-		return -1;
-	}
-	
 	/* encrypt packet body  */
- 	_tac_crypt(pkt, th, pkt_len);
+	_tac_crypt(pkt, th, pkt_len);
 
-	/* write body */
-	w=write(fd, pkt, pkt_len);
-	if(w < pkt_len) {
-		syslog(LOG_ERR, "%s: author body send failed: wrote %d of %d", 
-				__FUNCTION__, w,
-				pkt_len);
+	data = xcalloc(1, TAC_PLUS_HDR_SIZE + pkt_len);
+	d = data;
+	bcopy(th, d, TAC_PLUS_HDR_SIZE);
+	d += TAC_PLUS_HDR_SIZE;
+	bcopy(pkt, d, pkt_len);
+
+	/* send everything  */
+	w = write(fd, data, TAC_PLUS_HDR_SIZE + pkt_len);
+	if (w < (TAC_PLUS_HDR_SIZE + pkt_len)) {
+		syslog(LOG_ERR, "%s: author send failed: wrote %d of %d bytes", __FUNCTION__, w, pkt_len);
 		ret = -1;
 	}
 
+	free(data);
 	free(pkt);
 	free(th);
 
-	return(ret);
+	return (ret);
 }
